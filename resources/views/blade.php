9dc1220591a02d53412054f644de1fe4f9afcd37
aknaik99 <aknaik99@gmail.com>
2020-05-22 14:20:13 -0400

    current front end files. (nav bar with logo)

---
+++ resources/views/blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="em">
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Drop Shop</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+	<!-- CSS -->
+
+	<link href="dropshop.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+	<header>
+		<nav class="navbar navbar-expand-md bg-light navbar-light">
+			
+
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#"><img src="DSlogo.jpg" class="img-responsive logo"></a>
+		</div>
+
+
+ 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    		<span class="navbar-toggler-icon"></span>
+  		</button>
+
+	 	<div class="collapse navbar-collapse justify-content-center" id="navbarItems">
+    		<ul class="navbar-nav" id="navbarItems">
+      			<li class="nav-item">
+        			<a class="nav-link" href="#">Home</a>
+      			</li>
+      			<li class="nav-item">
+        			<a class="nav-link" href="#">Products</a>
+      			</li>
+      			<li class="nav-item">
+        			<a class="nav-link" href="#">Calendar</a>
+      			</li>
+      			<li class="nav-item">
+        			<a class="nav-link" href="#">Contact Us</a>
+      			</li>
+    		</ul>
+  		</div>
+
+
+		
+		</nav>
+	</header>
+</body>
+</html>
