a533503f78405cc82dc732fe79ae88713cb2d35f
aknaik99 <aknaik99@gmail.com>
2020-05-27 10:57:49 -0400

    no message

--- resources/views/drop-shop.blade.php
+++ resources/views/drop-shop.blade.php
@@ -7,15 +7,24 @@
 
 	<title>Drop Shop</title>
 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
+  
 	<!-- CSS -->
 
 	<link href="{{ url('css/dropshop.css') }}" rel="stylesheet" type="text/css">
 
 </head>
+
 <body>
+
+	<!-- Navbar -->
+
 	<header>
 		<nav class="navbar navbar-expand-md bg-light navbar-light">
 			
@@ -44,11 +53,188 @@
         			<a class="nav-link" href="#">Contact Us</a>
       			</li>
     		</ul>
+        
+        <!-- User icon button -->
+        <a href="#" class="btn btn-info btn-lg btn-light">
+          <span class="glyphicon glyphicon-user"></span> User 
+        </a>
+  
   		</div>
 
 
 		
 		</nav>
+
 	</header>
+
+	<!-- Slideshow -->
+<div class="slideshow-container">
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 10</div>
+    <img src="/img/j1.jpg" style="width:100%">
+    <div class="text">Travis x Jordan 1's</div>
+  </div>
+
+  <div class="mySlides fade-out">
+    <div class="numbertext">1 / 10</div>
+    <img src="/img/j1.jpg" style="width:100%">
+    <div class="text">Travis x Jordan 1's</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 10</div>
+    <img src="/img/ferg.jpg" style="width:100%">
+    <div class="text">A$AP Ferg x Jordan</div>
+  </div>
+
+  <div class="mySlides fade-out">
+    <div class="numbertext">2 / 10</div>
+    <img src="/img/ferg.jpg" style="width:100%">
+    <div class="text">A$AP Ferg x Jordan</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 10</div>
+    <img src="/img/ferg1.jpg" style="width:100%">
+    <div class="text">A$AP Ferg x Nike</div>
+  </div>
+
+  <div class="mySlides fade-out">
+    <div class="numbertext">3 / 10</div>
+    <img src="/img/ferg1.jpg" style="width:100%">
+    <div class="text">A$AP Ferg x Nike</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">4 / 10</div>
+    <img src="/img/puma.jpg" style="width:100%">
+    <div class="text">Puma x GunnerStahl</div>
+  </div>
+
+  <div class="mySlides fade-out">
+    <div class="numbertext">4 / 10</div>
+    <img src="/img/puma.jpg" style="width:100%">
+    <div class="text">Puma x GunnerStahl</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">5 / 10</div>
+    <img src="/img/russell.jpg" style="width:100%">
+    <div class="text">Westbrook x Jordan</div>
+  </div>
+
+  <div class="mySlides fade-out">
+    <div class="numbertext">5 / 10</div>
+    <img src="/img/russell.jpg" style="width:100%">
+    <div class="text">Westbrook x Jordan</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">6 / 10</div>
+    <img src="/img/t5.jpg" style="width:100%">
+    <div class="text">Travis x Jordan</div>
+  </div>
+
+  <div class="mySlides fade-out">
+    <div class="numbertext">6 / 10</div>
+    <img src="/img/t5.jpg" style="width:100%">
+    <div class="text">Travis x Jordan</div>
+  </div>
+
+<div class="mySlides fade">
+    <div class="numbertext">7 / 10</div>
+    <img src="/img/tc.jpg" style="width:100%">
+    <div class="text">Tyler the Creator x Converse</div>
+  </div>
+
+  <div class="mySlides fade-out">
+    <div class="numbertext">7 / 10</div>
+    <img src="/img/tc.jpg" style="width:100%">
+    <div class="text">Tyler the Creator x Converse</div>
+  </div>
+
+<div class="mySlides fade">
+    <div class="numbertext">8 / 10</div>
+    <img src="/img/tj.jpg" style="width:100%">
+    <div class="text">Travis x Jordan 7's</div>
+  </div>
+
+  <div class="mySlides fade-out">
+    <div class="numbertext">8 / 10</div>
+    <img src="/img/tj.jpg" style="width:100%">
+    <div class="text">Travis x Jordan 7's</div>
+  </div>
+
+<div class="mySlides fade">
+    <div class="numbertext">9 / 10</div>
+    <img src="/img/tscott2.jpg" style="width:100%">
+    <div class="text">Travis x Nike</div>
+  </div>
+
+<div class="mySlides fade-out">
+    <div class="numbertext">9 / 10</div>
+    <img src="/img/tscott2.jpg" style="width:100%">
+    <div class="text">Travis x Nike</div>
+  </div>
+
+ <div class="mySlides fade">
+    <div class="numbertext">10 / 10</div>
+    <img src="/img/tscott3.jpg" style="width:100%">
+    <div class="text">Travis x Dior x Jordan</div>
+  </div>
+
+   <div class="mySlides fade-out">
+    <div class="numbertext">10 / 10</div>
+    <img src="/img/tscott3.jpg" style="width:100%">
+    <div class="text">Travis x Dior x Jordan</div>
+  </div>
+
+
+  <!-- Next and previous buttons -->
+  <!-- <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+ -->
+</div>
+<!-- <br> -->
+
+<!-- The dots/circles -->
+<!-- <div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+  <span class="dot" onclick="currentSlide(4)"></span>
+  <span class="dot" onclick="currentSlide(5)"></span>
+  <span class="dot" onclick="currentSlide(6)"></span>
+  <span class="dot" onclick="currentSlide(7)"></span>
+  <span class="dot" onclick="currentSlide(8)"></span>
+  <span class="dot" onclick="currentSlide(9)"></span>
+  <span class="dot" onclick="currentSlide(10)"></span>
+</div> -->
+
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+</script>
+
+<div class="container-fluid">
+  <h2 style="color: black; font-size: 45px;">About Us</h2><br>
+  <p style="color: black; font-size: 21px;">We strive to provide the SneakerHead community with a streamline hub that consists of product availability, price comaprisons, and release information.</p>
+
+</div>
+
 </body>
 </html>
