348fe043d152d32d441256e0ed5298258b6b09ef
aknaik99 <aknaik99@gmail.com>
2020-06-17 15:20:06 -0400

    created navbar and head template thanks to ryguy. implemented the template in the products page.

---
+++ resources/views/Template/navbar.blade.php
@@ -0,0 +1,38 @@
+
+	<header>
+
+		<!-- NAV BAR -->
+		<nav class="navbar navbar-expand-md bg-light navbar-light" role="navigation">
+
+
+        <div class="navbar-header">
+             <a class="navbar-brand" href="{{ route('drop-shop') }}"><img src="{{ url('img/DSlogo.jpg') }}" class="img-responsive logo"></a>
+        </div>
+
+
+         <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-controls="collapsibleNavbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+         <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
+            <ul class="navbar-nav" id="navbarItems">
+
+                  <li class="nav-item ml-auto">
+                    <a class="nav-link" href="{{ route('products') }}">PRODUCTS</a>
+                  </li>
+                  <li class="nav-item ml-auto">
+                    <a class="nav-link" href="{{ route('calendar') }}">CALENDAR</a>
+                  </li>
+                  <li class="nav-item ml-auto">
+                    <a class="nav-link" href="{{ route('contact') }}">CONTACT</a>
+                  </li>
+            <!-- User icon button -->
+              <li>
+                  <a href="" class=" nav-link btn btn-info btn-lg btn-light" id="User-icon">
+                  <span class="glyphicon glyphicon-user"></span> 
+                  </a>
+              </li>
+            </ul>
+      </div>
+        </nav>
+	</header>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,65 +1,8 @@
 <!DOCTYPE html>
 <html lang="em">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-	<title>Drop Shop  - Products</title>
-
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-
-  
-	<!-- CSS -->
-
-	<link href="{{ url('css/dropshop.css') }}" rel="stylesheet" type="text/css">
-</head>
+@include('template/headtag', ['title' => 'Drop Shop Products'])
 <body>
-	<header>
-
-		<!-- NAV BAR -->
-		<nav class="navbar navbar-expand-md bg-light navbar-light" role="navigation">
-
-
-        <div class="navbar-header">
-             <a class="navbar-brand" href="{{ route('drop-shop') }}"><img src="{{ url('img/DSlogo.jpg') }}" class="img-responsive logo"></a>
-        </div>
-
-
-         <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-controls="collapsibleNavbar" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-         <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
-            <ul class="navbar-nav" id="navbarItems">
-
-                  <li class="nav-item ml-auto">
-                    <a class="nav-link" href="{{ route('products') }}">PRODUCTS</a>
-                  </li>
-                  <li class="nav-item ml-auto">
-                    <a class="nav-link" href="{{ route('calendar') }}">CALENDAR</a>
-                  </li>
-                  <li class="nav-item ml-auto">
-                    <a class="nav-link" href="{{ route('contact') }}">CONTACT</a>
-                  </li>
-            <!-- User icon button -->
-              <li>
-                  <a href="" class=" nav-link btn btn-info btn-lg btn-light" id="User-icon">
-                  <span class="glyphicon glyphicon-user"></span> 
-                  </a>
-              </li>
-            </ul>
-      </div>
-        </nav>
-	</header>
+@include('template/navbar')
 
 
 	<!-- PRODUCTS Heading--> 
@@ -206,9 +149,50 @@
 
 
 </body>
-<footer class="footer">
-  <div class="panel-footer" align="center">
-    <span class="text-muted" align="center" style="font-size: 14px;">Derived From The Community, For The Community.</span>
+<!-- Footer -->
+<footer class="page-footer">
+
+  <!-- Footer Elements -->
+  <div class="container">
+
+    <!-- Social buttons -->
+    <ul>
+      <li>
+        <a class="btn-floating btn-fb mx-1">
+          <i class="fab fa-facebook-f"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-tw mx-1">
+          <i class="fab fa-twitter"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-gplus mx-1">
+          <i class="fab fa-google-plus-g"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-li mx-1">
+          <i class="fab fa-linkedin-in"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-dribbble mx-1">
+          <i class="fab fa-dribbble"> </i>
+        </a>
+      </li>
+    </ul>
+    <!-- Social buttons -->
+
   </div>
+  <!-- Footer Elements -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+
 </footer>
 </html>
