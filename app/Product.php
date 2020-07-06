4bd6feed447ed27b902ae1b6a027df52856588d8
DutCHer08 <josered28@gmail.com>
2020-07-06 17:57:56 -0400

    Updated Product model and scraperInput model to help pass urls to the collab_product view. Added a foreign key to products table.

--- app/Product.php
+++ app/Product.php
@@ -7,15 +7,20 @@ use Illuminate\Notifications\Notifiable;
 
 class Product extends Model
 {
-	  use Notifiable;
-    
+      use Notifiable;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [];
+
     public function getModalNameAttribute () {
-    	return strtolower(str_replace(' ', '', $this->productName));
+        return strtolower(str_replace(' ', '', $this->productName));
     }
+
+        public function scraper_links() {
+            return $this->hasOne('App\ScraperInput', 'id');
+        }
 }
--- app/ScraperInput.php
+++ app/ScraperInput.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class ScraperInput extends Model
 {
     protected $table = 'scraper_input';
+
     protected $guarded = [];
+
+    public function product(){
+      return $this->belongsTo('App\Product', 'links_id');
+    }
 }
