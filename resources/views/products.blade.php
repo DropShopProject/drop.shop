d954878327dd4e6627c134e39bca19f8fbf4cd0b
DutCHer08 <josered28@gmail.com>
2020-07-06 20:33:51 -0400

    Cut the modal out of the Products view for now and added Wally's new nav bar into it too.

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -43,7 +43,7 @@
   <div class="container-fluid justify-content-center" id="container">
 
     <!-- PRODUCTS NAV BAR -->
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top" role="navigation" id="products-navbar">
+    <nav class="navbar navbar-expand-sm sticky-top" role="navigation" id="products-navbar">
 
        <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#products-bar" aria-controls="collapsibleNavbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -52,11 +52,11 @@
         <div class="collapse navbar-collapse justify-content-center" id="products-bar">
             <ul class="navbar-nav" id="navbarItems-tab">
                   <li class="nav-item ml-auto dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="athleteDrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ATHLETES
+                    <a class="nav-link dropdown-toggle mr-auto" href="#" id="athleteDrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ATHLETES
                     </a>
                     <div class="dropdown-menu" aria-labelledby="athleteDrop">
-                      <a class="dropdown-item" href="#">KOBE BRYANT</a>
-                      <a class="dropdown-item" href="#">RAY ALLEN
+                      <a class="dropdown-item mx-auto" href="{{ route('products', ['collab_name' => 'kobe']) }}">KOBE BRYANT</a>
+                      <a class="dropdown-item mx-auto" href="{{ route('products', ['collab_name' => 'ray-allen']) }}">RAY ALLEN
                       </a>
                     </div>
                   </li>
@@ -64,19 +64,19 @@
                     <a class="nav-link dropdown-toggle" href="#" id="celebDrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">CELEBRITIES
                     </a>
                     <div class="dropdown-menu justify-content-center" aria-labelledby="celebDrop">
-                      <a class="dropdown-item" href="#">TRAVIS SCOTT
+                      <a class="dropdown-item" href="{{ route('products', ['collab_name' => 'travis-scott']) }}">TRAVIS SCOTT
                       </a>
-                      <a class="dropdown-item" href="#">A$AP ROCKY
+                      <a class="dropdown-item" href="{{ route('products', ['collab_name' => 'asap-rocky']) }}">A$AP ROCKY
                       </a>
-                      <a class="dropdown-item" href="#">YEEZY
+                      <a class="dropdown-item" href="{{ route('products', ['collab_name' => 'yeezy']) }}">YEEZY
                       </a>
-                      <a class="dropdown-item" href="#">RIHANNA
+                      <a class="dropdown-item" href="{{ route('products', ['collab_name' => 'rihanna']) }}">RIHANNA
                       </a>
-                      <a class="dropdown-item" href="#">PHARRELL
+                      <a class="dropdown-item" href="{{ route('products', ['collab_name' => 'pharrell']) }}">PHARRELL
                       </a>
-                      <a class="dropdown-item" href="#">DRAKE
+                      <a class="dropdown-item" href="{{ route('products', ['collab_name' => 'drake']) }}">DRAKE
                       </a>
-                      <a class="dropdown-item" href="#">KENDRICK LAMAR
+                      <a class="dropdown-item" href="{{ route('products', ['collab_name' => 'kendrick-lamar']) }}">KENDRICK LAMAR
                       </a>
                     </div>
                   </li>
@@ -84,14 +84,14 @@
                     <a class="nav-link dropdown-toggle" href="#" id="designDrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">DESIGNERS
                     </a>
                     <div class="dropdown-menu" aria-labelledby="designDrop">
-                      <a class="dropdown-item" href="#">ALEXANDER WANG
+                      <a class="dropdown-item" href="{{ route('products', ['collab_name' => 'alex-wang']) }}">ALEXANDER WANG
                       </a>
-                      <a class="dropdown-item" href="#">JUST DON
+                      <a class="dropdown-item" href="{{ route('products', ['collab_name' => 'just-don']) }}">JUST DON
                       </a>
-                      <a class="dropdown-item" href="#">FEAR OF GOD</a>
-                      <a class="dropdown-item" href="#">OFF-WHITE
+                      <a class="dropdown-item" href="{{ route('products', ['collab_name' => 'fear-of-god']) }}">FEAR OF GOD</a>
+                      <a class="dropdown-item" href="{{ route('products', ['collab_name' => 'off-white']) }}">OFF-WHITE
                       </a>
-                      <a class="dropdown-item" href="#">SUPREME
+                      <a class="dropdown-item" href="{{ route('products', ['collab_name' => 'supreme']) }}">SUPREME
                       </a>
                     </div>
                   </li>
@@ -109,56 +109,12 @@
           {{$shoe->productName}} X {{$shoe->collab_name}}
         </figcaption>
         <figcaption class="figure-caption text-center p-3">
-          <button type="button" class="btn btn-primary bg-dark" data-toggle="modal" data-target="#{{$shoe->modalName}}">
-          Compare prices and sizes
-          </button>
         </figcaption>
         </figure>
       </div>
       @endforeach
     </div>
   </div>
-
-
-</section>
-  @foreach ($shoes as $shoe)
-  <div class="modal" id="{{$shoe->modalName}}">
-    <div class="modal-dialog modal-dialog-centered modal-md">
-      <div class="modal-content">
-
-              <!-- MOCHA HIGH MODAL HEADER -->
-      <div class="modal-header">
-        <h3 class="modal-title text-center">Lowest Prices Available:</h3>
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-      </div>
-
-              <!-- MOCHA HIGH MODAL BODY -->
-      <div class="modal-body">
-        <div class="container">
-          <h3 class="p-3 text-center">FlightClub Prices Starting At:</h3>
-          <a href="https://www.flightclub.com/air-jordan-1-high-og-ts-sp-sail-black-dark-mocha-806920" target="_blank">
-          <h4 class="text-center">&dollar;1300</h4>
-          </a>
-          <h3 class="p-3 text-center">StockX Prices Starting At:</h3>
-          <a href="https://stockx.com/air-jordan-1-retro-high-travis-scott" target="_blank">
-          <h4 class="text-center">&dollar;1090</h4>
-          </a>
-          <h3 class="p-3 text-center">Goat Prices Starting At:</h3>
-          <a href="https://www.goat.com/sneakers/travis-scott-x-air-jordan-1-retro-high-og-cd4487-100" target="_blank">
-          <h4 class="text-center">&dollar;1090</h4>
-          </a>
-        </div>
-      </div>
-
-      <!-- MODAL FOOTER -->
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-      </div>
-
-    </div>
-  </div>
-</div>
-@endforeach
 
 @include('template/footer')
 
