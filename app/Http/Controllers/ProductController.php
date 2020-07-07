a361e1eac460d8c7781ac91a22e0c7d4d76d9946
DutCHer08 <josered28@gmail.com>
2020-07-06 20:18:06 -0400

    Cleaned up Product Controller's dead function and adding exception handling when there are no shoes for a collab.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,16 +7,12 @@ use \App\Product;
 
 class ProductController extends Controller
 {
-    public function show_product_details() {
-
-        $TestShoe = Product::where('id', '=', '1')->get();
-        $travisShoes = Product::where('productName', '=', 'TRAVIS')->get();
-
-        return view('products')->with('TestShoe', $TestShoe[0]);
-    }
-
     public function show_collab_products($collab_name) {
         $shoes = Product::where('collab_name', '=', $collab_name)->get();
+        if (count($shoes) < 1){
+          return redirect()->route('drop-shop');
+        }
+
         return view('collab_products')->with('shoes', $shoes);
     }
     
@@ -25,4 +21,3 @@ class ProductController extends Controller
     	return view('products')->with('shoes', $shoes);
     }
 }
- 
