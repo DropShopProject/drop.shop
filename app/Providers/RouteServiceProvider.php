5fe170d0cff11b1a1ea880a4e89f3a5b22ec95e4
zdavies98 <zdavies98@gmail.com>
2020-07-08 12:54:27 -0400

    Changed routes for login pages to the home page. Account page detail update

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,6 +22,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const ACCOUNT = '/account';
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, etc.
